5d0b2eb976e22d91e8a4b1accdae7ec0c4e67400
Jason Mei <koaaue@gmail.com>
2015-02-08 18:19:55 -0600

    Feb 8th, news agg

---
+++ MvcApplication8/MvcApplication8/Models/like.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication8.Models
+{
+    public class like
+    {
+        //public int Id { get; set; }
+        [Key]
+        [Column(Order = 0)]
+        public string UserName { get; set; }
+        [Key]
+        [Column(Order = 1)]
+        public int ItemId { get; set; }
+
+        //public virtual UserProfile UserProfile { get; set; }      //cannot link to the UserProfile?
+
+        public like()
+        {
+        }
+        public like(string n, int iid) {
+            UserName = n;      //foreign key can't use
+            ItemId = iid;
+        }
+    }
+
+}
---
+++ MvcApplication8/MvcApplication8/Scripts/upvote.js
@@ -0,0 +1,40 @@
+﻿function ClickUpvote(obj) {
+    //alert("alert test.");
+
+    var Li = {
+        UserName: "",
+        ItemId: obj
+    }
+    $.ajax({
+        type: "POST",
+        url: "/Xml2Model/Index",
+        data: Li}
+    );
+    
+
+    document.getElementById(obj).innerHTML = parseInt(document.getElementById(obj).innerHTML)+1;     //只能改变第一行啊
+    document.getElementById("i"+obj).onclick=null;
+
+}
+
+//===AJAX, 后台访问url并修改数据库===
+$(document).ready(function () {
+
+
+    $("#upvote-ajax").on("click", function () {
+        var Li = {
+            UserName: "ab",
+            ItemId: 13
+        }
+       
+        $.ajax({
+            type: "POST",
+            url: "/Xml2Model/Index",
+            data: Li/*,
+            success: function (data) {
+            }*/
+        });
+    });
+
+});
+
