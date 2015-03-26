025f33bfe4d5234fae8d43b9036c578a4b183e6c
Zebin Xie <zebin.xie@gmail.com>
2015-03-25 19:23:52 -0500

    增加.gitignore，现在临时文件不会加入版本库了。

--- NewsFeed/NewsFeed/Scripts/articleDetail.js
+++ NewsFeed/NewsFeed/Scripts/articleDetail.js
@@ -1,30 +1,17 @@
 ﻿
-var validateField = function (fieldElem, infoMessage, validateFn) {
-    // TODO: Implement validateField.
-
-    fieldElem.next().removeClass();
-
-    if (fieldElem.val() == "") {
-
-        fieldElem.next().text(infoMessage);             //先创建好span标签，然后.next().text()修改内容
-        fieldElem.next().addClass("info");				//.next().addClass()修改css样式
-    }
-};
-
-
 $(document).ready(function () {
     // TODO: Use validateField to validate form fields on the page.
 
-    //var info;
+    //$(".hidden").each().hide();
 
-    /*focus进入焦点时触发；blur失去焦点时触发*/
     $(".desc").click(function () {                                  //click
         $(".hidden").each(function () {                             //效果比blur好
             if ($(this).css("display") != "none")
                 $(this).css("display", "none");
         });
 
-        $(this).parent().next().css("display", "inline");          //display很乱
+        $(this).parent().next().css("display", "");          //display很乱, 手机display乱
+        
     });
 
     /*$(".desc").blur(function () {
